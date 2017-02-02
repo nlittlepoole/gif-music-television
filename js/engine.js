b5ea5c00d9897e5802b1a6a3fa9a3766d5b07310
Niger Little-Poole <nlittlepoole@gmail.com>
2017-02-01 21:10:07 -0500

    updated js

--- js/engine.js
+++ js/engine.js
@@ -6,6 +6,20 @@ function preload(arrayOfImages) {
     });
 }
 
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
 function gif_theme(theme){
     off = Math.floor(Math.random() * 4000)
     endpoint = 'https://api.giphy.com/v1/gifs/search?q=' + theme + '&api_key=dc6zaTOxFJmzC&offset=' + off + '&limit=10'
